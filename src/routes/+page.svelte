2258e51400896ef4f0adecd12237460961098480
Jenrite <vampirejoe2005@gmail.com>
2023-07-26 09:49:52 +1200

    DONE :)

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,5 @@
 <h1>Welcome to my site!!!</h1>
+<img src="https://media.tenor.com/M38huGqu3KEAAAAi/gold-cross.gif">
 <h2>LAWS OF THE LAND</h2>
 <h3>NO</h3>
 <ul>
@@ -7,6 +8,7 @@
     <li>scheming gnomes</li>
     <li>plump puppies</li>
 </ul>
-<img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Durer%2C_apocalisse%2C_12_il_mostro_marino_e_la_bestia.jpg/800px-Durer%2C_apocalisse%2C_12_il_mostro_marino_e_la_bestia.jpg">
-
+<div>
+    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/ec/Durer%2C_apocalisse%2C_12_il_mostro_marino_e_la_bestia.jpg/800px-Durer%2C_apocalisse%2C_12_il_mostro_marino_e_la_bestia.jpg">
+</div>
 <a href="https://www.op.ac.nz/" target="_blank">Otago polytech</a>
